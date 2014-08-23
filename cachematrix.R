bcabfe3bd591514a849bd0891fd75c1b55f4b584
carlosthomaz <cthomaz@storm.local>
2014-08-22 23:19:06 -0600

    1st commit
    1st commit - code still under development / debugging

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  c_matrix <- NULL
+  set < function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setmatrix <- function(solve) c_matrix <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
+  
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  c_matrix <- x$getmatrix()
+  if (!is.null(c_matrix)) {
+    message("cached data")
+    return(c_matrix)
+  }
+  cm <- x$get()
+  c_matrix <- solve(cm, ...)
+  x$setmatrix(c_matrix)
+  c_matrix
 }
